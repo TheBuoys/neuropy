c41196daf96efa4381f682d04e6b4af16b8b610d
Stephen Vondenstein <stephen@vondenstein.com>
2019-06-12 22:33:35 -0500

    Finalize setup.py. As a result, rewrite schema loading to load from 'package_data' provided by setuptools.

--- neuropy/utility/path.py
+++ neuropy/utility/path.py
@@ -16,8 +16,6 @@ import os
 
 class Path(argparse.Action):
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
-        # if nargs is not None:
-        #     raise ValueError("nargs not allowed")
         super(Path, self).__init__(option_strings, dest, **kwargs)
 
     def __call__(self, parser, namespace, values, option_string=None):
